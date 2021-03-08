59f4ef520e3a7f4ba141c1112bb2d02d0d501cc9
Grace-Malae <grace.t.malae@gmial.com>
2021-03-08 13:29:17 +1300

    dress the clown and whack a mole update

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -2,4 +2,67 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+// push the left and right arrow keys to change the outfit
+//push the up and down arrow keys to change which part of the body we're changing
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+  if (e.keyCode == '38'){
+    //up
+    changeVertical(-1)
+  }
+  else if (e.keyCode == '40'){
+    //down
+    changeVertical(1)
+
+  }
+  else if (e.keyCode == '37'){
+    //left
+    changeHorizontal(-1)
+  }
+  else if (e.keyCode == '39'){
+    //right
+    changeHorizontal(1)
+  }
+}
+
+var indexes = [0, 0, 0]
+var mainIndex = 0
+var head = document.getElementById("head")
+var body = document.getElementById("body")
+var shoes = document.getElementById("shoes")
+var imgs = [head, body, shoes]
+var strings = ["head", "body", "shoes"]
+
+
+function changeHorizontal(shift) {
+  
+  var index = indexes[mainIndex]
+  var image = imgs[mainIndex]
+  var str = strings[mainIndex]
+
+  index += shift
+
+  if (index < 0){
+    index = 5
+  }
+  if (index > 5){
+    index = 0
+  }
+  indexes[mainIndex] = index
+  image.src = "./images/"+ str + index + ".png"
+}
+
+
+function changeVertical(shift){
+  mainIndex += shift
+
+  if (mainIndex < 0)
+    mainIndex = 2
+  
+  if (mainIndex > 2)
+    mainIndex = 0
+  
+}
--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -2,4 +2,25 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+// get a random cell
+// mole image appears in random cell
+// click on the mole
+// mole respawns in new random cell
+
+var cells = document.getElementsByTagName("td")
+var randomIndex = Math.floor(Math.random() * cells.length); 
+var randomCell = cells[randomIndex]
+var mole = document.createElement ('img');
+mole.src = './mole.PNG';
+mole.id = "mole"; 
+randomCell.appendChild(mole); 
+mole.onclick = whackedMole
+
+function whackedMole() {
+  randomIndex = Math.floor(Math.random() * cells.length);
+  randomCell = cells[randomIndex]
+  randomCell.appendChild(mole); 
+
+  var audio = new Audio('./whack-audio.wav');
+  audio.play()
+}
